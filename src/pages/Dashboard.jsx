c175859da1e7656708ccdbb315b9214493c59b5b
jchien123 <jacobwchien@gmail.com>
2025-01-12 15:43:36 +0100

    tutorial 4 finished grid layout

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,43 +1,61 @@
-import { Box, Container, Heading, Text } from '@chakra-ui/react';
-import { color } from 'framer-motion';
+import { Box, Container, Heading, SimpleGrid, Text } from '@chakra-ui/react';
+// import { color } from 'framer-motion';
 
 
 export default function Dashboard() {
 
-  const boxStyles = {
-    p: "10px", 
-    bg: "purple.400", 
-    color: "white",
-    m: "10px",
-    textAlign: "center",
-    filter: "blur(2px)", 
-    ':hover' : {
-      color: "black",
-      bg: 'blue.200',
-    }
-  }
+  // const boxStyles = {
+  //   p: "10px", 
+  //   bg: "purple.400", 
+  //   color: "white",
+  //   m: "10px",
+  //   textAlign: "center",
+  //   filter: "blur(2px)", 
+  //   ':hover' : {
+  //     color: "black",
+  //     bg: 'blue.200',
+  //   }
+  // }
 
   return (
-    <Container as = "section" maxWidth="md">
-      <Heading my = "30px" p = "10px"> 
-        Chakra UI Components 
-      </Heading>
 
-      <Text marginLeft = "30px" color = "green.600" fontWeight="bold"> 
-        something something something
-      </Text>
+    <SimpleGrid columns = {3} spacing = {10} minChildWidth= "250px">
+      <Box bg = "white" h = "200px" border = "1px solid"> </Box>
+      <Box bg = "white" h = "200px" border = "1px solid"> </Box>
+      <Box bg = "white" h = "200px" border = "1px solid"> </Box>
+      <Box bg = "white" h = "200px" border = "1px solid"> </Box>
 
-      <Box my = "30px" p = "10px" bg = "blue.100">
-        <Text color = "black  ">
-          This is a box.
-        </Text>
-      </Box>
+      <Box bg = "white" h = "200px" border = "1px solid"> </Box>
+      <Box bg = "white" h = "200px" border = "1px solid"> </Box>
+      <Box bg = "white" h = "200px" border = "1px solid"> </Box>
+      <Box bg = "white" h = "200px" border = "1px solid"> </Box>
 
-      <Box sx = {boxStyles}> 
-        Hello, World!
-      </Box>
+      <Box bg = "white" h = "200px" border = "1px solid"> </Box>
+      <Box bg = "white" h = "200px" border = "1px solid"> </Box>
+      <Box bg = "white" h = "200px" border = "1px solid"> </Box>
+      <Box bg = "white" h = "200px" border = "1px solid"> </Box>
+    </SimpleGrid>
 
-    </Container>
+    // <Container as = "section" maxWidth="md">
+    //   <Heading my = "30px" p = "10px"> 
+    //     Chakra UI Components 
+    //   </Heading>
+
+    //   <Text marginLeft = "30px" color = "green.600" fontWeight="bold"> 
+    //     something something something
+    //   </Text>
+
+    //   <Box my = "30px" p = "10px" bg = "blue.100">
+    //     <Text color = "black  ">
+    //       This is a box.
+    //     </Text>
+    //   </Box>
+
+    //   <Box sx = {boxStyles}> 
+    //     Hello, World!
+    //   </Box>
+
+    // </Container>
 
   )
 }
