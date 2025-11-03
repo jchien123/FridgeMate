93682b9dfdb39b0e26f371333ba8d14cba7e610b
jchien123 <jacobwchien@gmail.com>
2025-11-03 02:03:20 -0500

    redeployed backend

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -71,7 +71,7 @@ export default function Create() {
 
       debugLog('Sending create request', createRequest);
 
-      const response = await fetch('https://motionless-karyn-jacobspersonal-0432e84b.koyeb.app/api/food', {
+      const response = await fetch('https://monetary-narwhal-fridgemate-b19bb297.koyeb.app/api/food', {
         method: 'POST',
         headers: {
           'Authorization': `Bearer ${token}`,
